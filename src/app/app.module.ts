cc0ba86651ca8189844ec38a3a83ab74e058f106
myuxguide <myuxguide@gmail.com>
2022-05-11 14:45:35 +0530

    Added reactive Form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +16,8 @@ import { UsersModule } from './users/users.module';
     AppRoutingModule,
     UsersModule,
     HttpClientModule,
-    UserFormsModule
+    UserFormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user-forms/components/user-forms/user-forms.component.ts
+++ src/app/user-forms/components/user-forms/user-forms.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { UserFormsModel } from '../../user0-forms.model';
+import { FormGroup, FormControl, Validators, FormArray, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-user-forms',
@@ -8,10 +7,108 @@ import { UserFormsModel } from '../../user0-forms.model';
   styleUrls: ['./user-forms.component.scss']
 })
 export class UserFormsComponent implements OnInit {
-  constructor() {}
+  title="Registration Form";
+  reactiveRegistrationForms:FormGroup;
 
-  ngOnInit(): void {
-    
+  // "formInfo": [
+  //   {
+  //       "formType": "inputText",
+  //       "type": "text",
+  //       "id":"floatingFirstName",
+  //       "formControlName":"firstName",
+  //       "maxlength":25,
+  //       "autocomplete":"off",
+  //       "label":"First Name"
+  //   },
+  //   {
+  //       "formType": "inputText",
+  //       "type": "text",
+  //       "id":"floatingLastName",
+  //       "formControlName":"lastName",
+  //       "maxlength":25,
+  //       "autocomplete":"off",
+  //       "label":"Last Name"
+  //   }
+  // ]
+
+  constructor(private formBuilder:FormBuilder) {
+    this.reactiveRegistrationForms = new FormGroup({
+      formArrays: this.formBuilder.array([])
+    })
+
+    this.buildForm();
   }
 
+  buildForm(){
+    const controlArray = this.reactiveRegistrationForms.get('formArrays') as FormArray;
+
+    // Object.keys(this.formInfo).forEach((i) => {
+    //   controlArray.push(
+    //     this.formBuilder.group({
+    //       formType: new FormControl({ value: this.formInfo[i].formType, disabled: true })
+    //     })
+    //   )
+    // })
+  }
+
+  ngOnInit(): void {
+    this.reactiveRegistrationForms = new FormGroup({
+      firstName: new FormControl(null, [Validators.required]),
+      lastName: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      courseName: new FormControl(null, Validators.required),
+      gender: new FormControl(null, Validators.required),
+      countryCode: new FormControl(null,Validators.required),
+      dob: new FormControl(null, Validators.required),
+      phone: new FormControl(('+91'), [Validators.required]),
+      addr: new FormControl(null, Validators.required)
+    })
+  }
+
+   
+
+    
+    // "options": [
+    //     {
+    //         "value": "courses",
+    //         "optionList": "Select Course",
+    //         "selectedAttr": "selected"
+    //     },
+    //     {
+    //         "value": "bca",
+    //         "options": "BCA",
+    //         "selectedAttr": ""
+    //     },
+    //     {
+    //         "value": "bba",
+    //         "options": "BBA",
+    //         "selectedAttr": ""
+    //     },
+    //     {
+    //         "value": "bTech",
+    //         "options": "B.Tech",
+    //         "selectedAttr": ""
+    //     },
+    //     {
+    //         "value": "mba",
+    //         "options": "MBA",
+    //         "selectedAttr": ""
+    //     },
+    //     {
+    //         "value": "mca",
+    //         "options": "MCA",
+    //         "selectedAttr": ""
+    //     },
+    //     {
+    //         "value": "mTech",
+    //         "options": "M.Tech",
+    //         "selectedAttr": ""
+    //     }
+    // ]
+
+
+  onSubmit(){
+    console.log(this.reactiveRegistrationForms);
+    
+  }
 }
