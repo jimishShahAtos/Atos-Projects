7dc87d27b6fb4afd9e748f54e7716e21ef0d7cf6
myuxguide <myuxguide@gmail.com>
2022-06-09 15:25:19 +0530

    Added new project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './users/components/users/dashboard/dashboard.component';
-import { LoginComponent } from './users/components/users/login/login.component';
 
-const routes: Routes = [
-  {
-    path : "",
-    redirectTo:"DashboardComponent",
-    pathMatch: "full"
-  },
-  { 
-    path: 'login', 
-    component: LoginComponent
-  },
-  { 
-    path: 'dashboard', 
-    component: DashboardComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UsersModule } from './users/users.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,10 +10,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    UsersModule,
-    HttpClientModule,
-    BrowserAnimationsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
